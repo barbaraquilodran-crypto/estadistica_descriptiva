a5d2dea148e3c3927d94a0349b6535d74db11bd5
barbaraquilodran-crypto <barbara.quilodran@usach.cl>
2026-01-04 13:06:14 -0300

    Add files via upload

---
+++ __init__.py
@@ -0,0 +1,6 @@
+from .promedio import promedio
+from .mediana import mediana
+from .desviacion_estandar import desviacion_estandar
+from .rango_intercuartilico import rango_intercuartilico
+from .moda import moda
+from .percentil import percentil
